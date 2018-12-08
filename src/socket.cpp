62b89182ea920b19753ddf32c6229654d472679f
trosemberg <tomasroro@hotmail.com>
2018-12-08 14:34:45 -0200

    COMEÇAR A FAZER RESPONSE EDITAVEL

--- src/socket.cpp
+++ src/socket.cpp
@@ -128,6 +128,7 @@ void Socket::sendToClientSocket(const char* bufferServer,int socketfd,int sizeBu
 }
 
 // recebe de volta do servidor a resposta
+// TODO FAZER RESPONSE SER EDITAVEL
 void Socket::receiveFromServer (int Clientfd, int Serverfd) {
 	std::string temp;
 	int sizeBuffer = 5000;
