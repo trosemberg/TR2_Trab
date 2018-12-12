23e4b46e0509896b27fd932e277428c2336c2de4
trosemberg <tomasroro@hotmail.com>
2018-12-12 08:24:35 -0200

    resolvido referencias logicas + moved to

--- src/crawler.cpp
+++ src/crawler.cpp
@@ -29,7 +29,7 @@ int Crawler::wget (char *host, char *path, int atual, int max){
     // std::cout<<"ENTER WGET";
     while (found!=std::string::npos){
         caminho[found]='_';
-        found=caminho.find_first_of("/",found+1);
+        found=caminho.find_first_of("/",found);
     }
 
     FILE *fp1;
@@ -119,14 +119,17 @@ std::queue <std::string> Crawler::spider(char *host,char *path, int atual, int m
     char hoost[100] = "/", paath[100] = "/", temp[100],*temp_2;
     char dados[5000];
     std::fstream file;
+    std::string HTML = ".html";
     char arquivo[1000]="./html/";
     std::string URL = "";
     std::string caminho(path);
     std::size_t achou;
-    std::size_t found = caminho.find_first_of("/");
-    while (found!=std::string::npos){
-        caminho[found]='_';
-        found=caminho.find_first_of("/",found+1);
+    std::size_t found,found_2;
+    std::size_t achado = caminho.find_first_of("/");
+    std::string str2;
+    while (achado!=std::string::npos){
+        caminho[achado]='_';
+        achado=caminho.find_first_of("/",achado);
     }
     strcat(arquivo,host);
     strcat(arquivo,caminho.c_str());
@@ -163,6 +166,30 @@ std::queue <std::string> Crawler::spider(char *host,char *path, int atual, int m
             file.close();
         }
     }
+    file.open(arquivo,std::ios::in);
+	msg.assign((std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>());
+	file.close();
+    if(!msg.empty()){
+        found = msg.find("<a href=\"http://");
+        found_2 = msg.find("\"",found+10);
+        while(found!=std::string::npos){
+            if(found!=std::string::npos && found_2!=std::string::npos){
+                msg.erase(found+9,7);
+                found_2 = msg.find("\"",found+10);
+                if(found_2!=std::string::npos){
+                    str2 = msg.substr (found,found_2-found);
+                    std::replace( str2.begin(), str2.end(), '/', '_');
+                    str2+=HTML;
+                    msg.replace(found,found_2-found,str2);
+                }
+            }
+            found = msg.find("<a href=\"http://",found+1);
+        }
+    }
+    file.open(arquivo,std::ios::out);
+    file << msg;
+    file.close();
 
     while(!auxiliar.empty()){
         std::strcpy(hoost,"/");
@@ -179,6 +206,7 @@ std::queue <std::string> Crawler::spider(char *host,char *path, int atual, int m
             std::strcpy(paath,"/");
         }else{
             std::strcat(paath,temp_2);
+            std::strcat(paath,"/");
         }
         URL += hoost;
         URL += paath;
@@ -216,4 +244,4 @@ void Crawler::run(char *host,char *path){
 }
 
 
-// TODO: RESOLVER O HTTP 301(NÃO CONSEGUI), FAZER INTERFACE GRAFICA, RESOLVER REFERENCIAS LOCAIS.
+// TODO: RESOLVER O HTTP 301(NÃO CONSEGUI), FAZER INTERFACE GRAFICA
