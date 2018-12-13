ab59a31505abda76a7cd41526a104b81e518e941
trosemberg <tomasroro@hotmail.com>
2018-12-13 00:19:23 -0200

    teste

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "socket.hpp"
-#include "servidor.hpp"
+#include "header.hpp"
 #include "util.hpp"
 
 #define PORTA 8228
@@ -7,35 +7,29 @@
 // caso seja fornecida uma porta, usa ela, caso nao usar porta padrao 8228
 
 int main(int argc, char const *argv[]){
-  long int porta = 0;
-  std::string flag,porta_escolhida;
-  
-  switch (argc){
-    case 1:
-    porta = PORTA;
-      break;
-    case 3:
-      flag = argv[1];
-      porta = strtol(argv[2], nullptr, 10);
+	Socket socket((long int)8228);
+	HTTP http((long int)8228);
+	//Cria socket
+	socket.bind_socket = socket.SocketCreate();
+	if (socket.bind_socket == -1){
+		printf("Could not create socket");
+		return 1;
+	}
+	printf("Socket created\n");
+	//Faz o Bind e o Listen do socket
+	socket.SocketBind();
+	if( socket.bind_socket < 0){
+		//print the error message
+		perror("bind failed.");
+		return 1;
+	}
+	while(1){
+		socket.SocketAccept();
+		http.RequestHTTP((void*)&socket.inSocket);      
+	}
+	close(socket.inSocket);
+	close(socket.bind_socket);
+	printf("Bind e Listen feito \n");
 
-      if (porta < 1024 || porta > 65535) {
-        std::cout<<"\nO numero da porta deve estar no range valido de 1024 a 65535\n";
-        exit(-1);
-      }else if(flag.compare("-p") !=0){
-        std::cout<< "a flag deve ser -p\n";
-        exit(-1);
-      }
-      break;
-    default:
-      std::cout<< "O formato do programa deve ser:\n \"./aracne\" ou "<<std::endl;
-      std::cout<< "\"./aracne -p (numero da porta)\"\n";
-      exit(-1);
-      break;
-  }
-  std::cout<<"\n\nConectando a porta:"<< porta<<"...";
-  // cria servidor e inicializa ele com porta escolhida
-  Servidor server((long int)porta);
-  server.init();
-  server.run();
-  return 0;
+	return 0;
 }
